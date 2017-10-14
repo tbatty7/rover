276c48acf09388362133ad6ff8a05a9f32cdd2fc
Timothy Batty <tbatty7@gmail.com>
2017-10-13 23:25:05 -0400

    Added reverse movement function

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
+
+
+
 var Rover = function(x,y,direction){  // Does this need to have the parameter in the form of an array? If so, it would be the same, except the one parameter would be arr, and the parameter would be x = arr[0], y = arr[1], and direction = arr[2].
 
 	var self = {
 		x: 10,
 		x: 10,
-		direction: ‘N’,
+		direction: 'N',
 	}
 
 	if(x){          // This tests if there are any parameters in the function, and if so, uses them to create the rover object.
@@ -13,21 +17,36 @@ var Rover = function(x,y,direction){  // Does this need to have the parameter in
 	}
 
 	self.forward = function(){
-	       if (self.direction === ‘N’){
-		      self.x++;
+       if (self.direction === 'N'){
+	      self.x++;
 		}
-	       if (self.direction === ‘S’){
-	          self.x--;
+       if (self.direction === 'S'){
+          self.x--;
 		}
-	       if (self.direction === ‘E’){
-	          self.y++;
+       if (self.direction === 'E'){
+          self.y++;
 		}
-	       if (self.direction === ‘W’){
-	          self.y--;
+       if (self.direction === 'W'){
+          self.y--;
 		}
 	}
 
-	
+	self.backward = function(){
+       if (self.direction === 'N'){
+	      self.x--;
+		}
+       if (self.direction === 'S'){
+          self.x++;
+		}
+       if (self.direction === 'E'){
+          self.y--;
+		}
+       if (self.direction === 'W'){
+          self.y++;
+		}
+	}
+
+
 
 	return self;
 }
